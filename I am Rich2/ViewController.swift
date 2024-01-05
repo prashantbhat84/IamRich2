67b5e4ccb21e7e867d6196abe10274235892dcfa
Prashant Bhat <prashantbhat84@gmail.com>
2024-01-05 17:30:40 +0530

    Initial Commit

---
+++ I am Rich2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich2
+//
+//  Created by Prashant Bhat on 05/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
